e746a6e0cae5f57d69c090d29605eb03e6886734
maximyudayev <maxim.yudayev@gmail.com>
2021-11-12 17:18:10 +0100

    Commit to pull MAC changes

--- dve/src/device/top_chip.sv
+++ dve/src/device/top_chip.sv
@@ -1,46 +1,46 @@
 module top_chip #(
-    parameter int IO_DATA_WIDTH = 16,
-    parameter int ACCUMULATION_WIDTH = 32,
-    parameter int EXT_MEM_HEIGHT = 1<<20,
-    parameter int EXT_MEM_WIDTH = ACCUMULATION_WIDTH,
-    parameter int FEATURE_MAP_WIDTH = 1024,
-    parameter int FEATURE_MAP_HEIGHT = 1024,
-    parameter int INPUT_NB_CHANNELS = 64,
-    parameter int OUTPUT_NB_CHANNELS = 64,
-    parameter int KERNEL_SIZE = 3
+    parameter int IO_DATA_WIDTH         = 16,
+    parameter int ACCUMULATION_WIDTH    = 32,
+    parameter int EXT_MEM_HEIGHT        = 1<<20,
+    parameter int EXT_MEM_WIDTH         = ACCUMULATION_WIDTH,
+    parameter int FEATURE_MAP_WIDTH     = 1024,
+    parameter int FEATURE_MAP_HEIGHT    = 1024,
+    parameter int INPUT_NB_CHANNELS     = 64,
+    parameter int OUTPUT_NB_CHANNELS    = 64,
+    parameter int KERNEL_SIZE           = 3,
+    parameter int NUM_MAC_UNITS         = 1
   )
-  (input logic clk,
-   input logic arst_n_in,  //asynchronous reset, active low
+  (
+    input logic clk,
+    input logic arst_n_in,  //asynchronous reset, active low
 
-   //external_memory
-   //read port
-   output logic unsigned[$clog2(EXT_MEM_HEIGHT)-1:0] ext_mem_read_addr,
-   output logic ext_mem_read_en,
-   input logic[EXT_MEM_WIDTH-1:0] ext_mem_qout,
-
-   //write port
-   output logic unsigned[$clog2(EXT_MEM_HEIGHT)-1:0] ext_mem_write_addr,
-   output logic [EXT_MEM_WIDTH-1:0] ext_mem_din,
-   output logic ext_mem_write_en,
-
-   //system inputs and outputs
-   input logic [IO_DATA_WIDTH-1:0] a_input,
-   input logic a_valid,
-   output logic a_ready,
-   input logic [IO_DATA_WIDTH-1:0] b_input,
-   input logic b_valid,
-   output logic b_ready,
-
-   //output
-   output logic signed [IO_DATA_WIDTH-1:0] out,
-   output logic output_valid,
-   output logic [$clog2(FEATURE_MAP_WIDTH)-1:0] output_x,
-   output logic [$clog2(FEATURE_MAP_HEIGHT)-1:0] output_y,
-   output logic [$clog2(OUTPUT_NB_CHANNELS)-1:0] output_ch,
-
-
-   input logic start,
-   output logic running
+    //external memory read port
+    output logic unsigned[$clog2(EXT_MEM_HEIGHT)-1:0] ext_mem_read_addr,
+    output logic ext_mem_read_en,
+    input logic[EXT_MEM_WIDTH-1:0] ext_mem_qout,
+    
+    //external memory write port
+    output logic unsigned[$clog2(EXT_MEM_HEIGHT)-1:0] ext_mem_write_addr,
+    output logic [EXT_MEM_WIDTH-1:0] ext_mem_din,
+    output logic ext_mem_write_en,
+    
+    //system inputs and outputs
+    input logic [IO_DATA_WIDTH-1:0] a_input,
+    input logic a_valid,
+    output logic a_ready,
+    input logic [IO_DATA_WIDTH-1:0] b_input,
+    input logic b_valid,
+    output logic b_ready,
+    
+    //output
+    output logic signed [IO_DATA_WIDTH-1:0] out,
+    output logic output_valid,
+    output logic [$clog2(FEATURE_MAP_WIDTH)-1:0] output_x,
+    output logic [$clog2(FEATURE_MAP_HEIGHT)-1:0] output_y,
+    output logic [$clog2(OUTPUT_NB_CHANNELS)-1:0] output_ch,
+    
+    input logic start,
+    output logic running
   );
 
 
@@ -58,43 +58,38 @@ module top_chip #(
   logic mac_accumulate_internal;
   logic mac_accumulate_with_0;
 
-
+  /*
+  * controller
+  */
   controller_fsm #(
-  .LOG2_OF_MEM_HEIGHT($clog2(EXT_MEM_HEIGHT)),
-  .FEATURE_MAP_WIDTH(FEATURE_MAP_WIDTH),
-  .FEATURE_MAP_HEIGHT(FEATURE_MAP_HEIGHT),
-  .INPUT_NB_CHANNELS(INPUT_NB_CHANNELS),
-  .OUTPUT_NB_CHANNELS(OUTPUT_NB_CHANNELS),
-  .KERNEL_SIZE(KERNEL_SIZE)
-  )
-  controller
-  (.clk(clk),
-  .arst_n_in(arst_n_in),
-  .start(start),
-  .running(running),
-
-  .mem_we(ext_mem_write_en),
-  .mem_write_addr(ext_mem_write_addr),
-  .mem_re(ext_mem_read_en),
-  .mem_read_addr(ext_mem_read_addr),
-
-  .a_valid(a_valid),
-  .a_ready(a_ready),
-  .b_valid(b_valid),
-  .b_ready(b_ready),
-  .write_a(write_a),
-  .write_b(write_b),
-  .mac_valid(mac_valid),
-  .mac_accumulate_internal(mac_accumulate_internal),
-  .mac_accumulate_with_0(mac_accumulate_with_0),
-
-  .output_valid(output_valid),
-  .output_x(output_x),
-  .output_y(output_y),
-  .output_ch(output_ch)
-
-  );
-
+    .LOG2_OF_MEM_HEIGHT($clog2(EXT_MEM_HEIGHT)),
+    .FEATURE_MAP_WIDTH(FEATURE_MAP_WIDTH),
+    .FEATURE_MAP_HEIGHT(FEATURE_MAP_HEIGHT),
+    .INPUT_NB_CHANNELS(INPUT_NB_CHANNELS),
+    .OUTPUT_NB_CHANNELS(OUTPUT_NB_CHANNELS),
+    .KERNEL_SIZE(KERNEL_SIZE))
+  controller(
+    .clk(clk),
+    .arst_n_in(arst_n_in),
+    .start(start),
+    .running(running),
+    .mem_we(ext_mem_write_en),
+    .mem_write_addr(ext_mem_write_addr),
+    .mem_re(ext_mem_read_en),
+    .mem_read_addr(ext_mem_read_addr),
+    .a_valid(a_valid),
+    .a_ready(a_ready),
+    .b_valid(b_valid),
+    .b_ready(b_ready),
+    .write_a(write_a),
+    .write_b(write_b),
+    .mac_valid(mac_valid),
+    .mac_accumulate_internal(mac_accumulate_internal),
+    .mac_accumulate_with_0(mac_accumulate_with_0),
+    .output_valid(output_valid),
+    .output_x(output_x),
+    .output_y(output_y),
+    .output_ch(output_ch));
 
 
   logic signed [ACCUMULATION_WIDTH-1:0] mac_partial_sum;
@@ -103,23 +98,28 @@ module top_chip #(
   logic signed [ACCUMULATION_WIDTH-1:0] mac_out;
   assign ext_mem_dout = mac_out;
 
-  mac #(
-    .A_WIDTH(IO_DATA_WIDTH),
-    .B_WIDTH(IO_DATA_WIDTH),
-    .ACCUMULATOR_WIDTH(ACCUMULATION_WIDTH),
-    .OUTPUT_WIDTH(ACCUMULATION_WIDTH),
-    .OUTPUT_SCALE(0)
-  )
-  mac_unit
-  (.clk(clk),
-   .arst_n_in(arst_n_in),
-
-   .input_valid(mac_valid),
-   .accumulate_internal(mac_accumulate_internal),
-   .partial_sum_in(mac_partial_sum),
-   .a(a),
-   .b(b),
-   .out(mac_out));
+  /*
+  * MAC arrays
+  */
+  generate
+    genvar i;
+    for(i=0; i<NUM_MAC_UNITS; i=i+1)
+      mac #(
+        .A_WIDTH(IO_DATA_WIDTH),
+        .B_WIDTH(IO_DATA_WIDTH),
+        .ACCUMULATOR_WIDTH(ACCUMULATION_WIDTH),
+        .OUTPUT_WIDTH(ACCUMULATION_WIDTH),
+        .OUTPUT_SCALE(0))
+      mac_unit(
+        .clk(clk),
+        .arst_n_in(arst_n_in),
+        .input_valid(mac_valid),
+        .accumulate_internal(mac_accumulate_internal[i]),
+        .partial_sum_in(mac_partial_sum[i]),
+        .a(a[i]),
+        .b(b[i]),
+        .out(mac_out[i]));
+  endgenerate
 
   assign out = mac_out;
   assign ext_mem_din = mac_out;
--- dve/src/rtl_building_blocks/device/memory.sv
+++ dve/src/rtl_building_blocks/device/memory.sv
@@ -1,37 +1,44 @@
 //a simple pseudo-2 port memory (can read and write simultaneously)
 //Feel free to write a single port memory (inout data, either write or read every cycle) to decrease your bandwidth
 module memory #(
-  parameter int WIDTH = 16,
-  parameter int HEIGHT = 1,
-  parameter bit USED_AS_EXTERNAL_MEM// for area, bandwidth and energy estimation
+    parameter int WIDTH = 16,
+    parameter int HEIGHT = 1,
+    parameter bit USED_AS_EXTERNAL_MEM = 0 // for area, bandwidth and energy estimation
   )
   (
-  input logic clk,
-
-  //read port (0 cycle: there is no clock edge between changing the read_addr and the output)
-  input logic unsigned[$clog2(HEIGHT)-1:0] read_addr,
-  input logic read_en,
-  output logic[WIDTH-1:0] qout,
-
-  //write port (data is written on the rising clock edge)
-  input logic unsigned[$clog2(HEIGHT)-1:0] write_addr,
-  input logic [WIDTH-1:0] din,
-  input logic write_en
+    input logic clk,
+  
+    //read port
+    input logic unsigned[$clog2(HEIGHT)-1:0] read_addr,
+    input logic read_en,
+    output logic[WIDTH-1:0] qout,
+  
+    //write port
+    input logic unsigned[$clog2(HEIGHT)-1:0] write_addr,
+    input logic [WIDTH-1:0] din,
+    input logic write_en
   );
 
 
-  //storage
+  /*
+  * storage
+  */
   logic [WIDTH-1:0] data [0:HEIGHT-1];
 
-  always @ (posedge clk) begin
+  //synchronous writes
+  always @(posedge clk) begin
     if (write_en) begin
-        data[write_addr] <= din;
+      data[write_addr] <= din;
     end
   end
 
+  //asynchronous reads
   assign qout = read_en ? data[read_addr] :'x ;
 
-  //area logging
+
+  /*
+  * area logging
+  */
   initial begin
     #0;
     if(!USED_AS_EXTERNAL_MEM) begin
@@ -45,7 +52,9 @@ module memory #(
     end
   end
 
-  //energy logging:
+  /*
+  * energy logging
+  */
   always @(posedge clk) begin
     if(read_en)
       tbench_top.energy += WIDTH*(USED_AS_EXTERNAL_MEM?1:0.1);
