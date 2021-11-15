4b330ba62ed6eca90d1f60f439841c1227ff5ea5
maximyudayev <maxim.yudayev@gmail.com>
2021-11-15 12:52:07 +0100

    Hierarchical referencing inside 1 generate fails
    Note: Tried to instantiate adder tree stages according to the
          recommendation of Koen (TA), but issue stems from labeled
          generate loop not able to be indexed to hierarchically select
          encapsulated signals.

--- dve/src/rtl_building_blocks/device/adder_tree.sv
+++ dve/src/rtl_building_blocks/device/adder_tree.sv
@@ -1,3 +1,5 @@
+`define CEIL_DIV(q, d) (q+d-1)/d
+
 module adder_tree #(
     parameter int NUM_INPUTS  = 36,
     parameter int INPUT_WIDTH = 32
@@ -10,52 +12,55 @@ module adder_tree #(
     output logic signed [$clog2(NUM_INPUTS)+INPUT_WIDTH-1:0] out //single reduced sum value
   );
   
-  localparam ACCUMULATOR_WIDTH = $clog2(NUM_INPUTS)+INPUT_WIDTH;
+  localparam NUM_ADDER_STAGES = $clog2(NUM_INPUTS);
+  localparam ACCUMULATOR_WIDTH = NUM_ADDER_STAGES+INPUT_WIDTH;
   
-  //binary tree adder nets for each stage
-  generate
-    for(genvar n=0; n<=$clog2(NUM_INPUTS); n=n+1) begin : nets //7 nets for 6 stage tree, 0 <= n <= 6
-      logic signed [INPUT_WIDTH+n-1:0] sum [0:((NUM_INPUTS+(2**n)-1)/(2**n))-1];
-    end : nets
-  endgenerate
-
-  assign nets.sum[0] = in; //connect first net to input of adder tree
-  assign out = nets.sum[$clog2(NUM_INPUTS)]; //connect module output to output of adder tree
+  logic signed [ACCUMULATOR_WIDTH-1:0] temp [0:0]; //adder tree stage creates/outputs a bus
+  assign out = temp[0]; //slice that bus into a single 16b signal that MAC output expects
   
   //adder stages (not radix-2)
   //orphan elements get sign extended before next stage
   generate    
-    for(genvar i=0; i<$clog2(NUM_INPUTS); i=i+1)
-      adder_tree_stage #(
-        .NUM_INPUTS((NUM_INPUTS+(2**i)-1)/(2**i)), //number of inputs for that stage
-        .ACCUMULATOR_WIDTH(INPUT_WIDTH+i+1), //each stage has an extra bit (to account for double the range during add)
-        .INPUT_WIDTH(INPUT_WIDTH+i)) //input width for that stage
-      adder_stage(
-        .clk(clk),
-        .arst_n_in(arst_n_in),
-        .in(nets.sum[i]),
-        .out(nets.sum[i+1]));
+    for(genvar i=0; i<NUM_ADDER_STAGES; i=i+1) begin : genaddstg
+      
+      if(i==0) begin //first stage
+        logic signed [INPUT_WIDTH:0] sum [0:((NUM_INPUTS+(2**(i+1))-1)/(2**(i+1)))-1];
+        adder_tree_stage #(
+          .NUM_INPUTS((NUM_INPUTS+(2**i)-1)/(2**i)),
+          .ACCUMULATOR_WIDTH(INPUT_WIDTH+1),
+          .INPUT_WIDTH(INPUT_WIDTH))
+        adder_stage(
+          .clk(clk),
+          .arst_n_in(arst_n_in),
+          .in(in),
+          .out(sum));
+      end
+      
+      else if(i==NUM_ADDER_STAGES-1) begin //last stage
+        adder_tree_stage #(
+          .NUM_INPUTS((NUM_INPUTS+(2**i)-1)/(2**i)),
+          .ACCUMULATOR_WIDTH(INPUT_WIDTH+i+1),
+          .INPUT_WIDTH(INPUT_WIDTH+i))
+        adder_stage(
+          .clk(clk),
+          .arst_n_in(arst_n_in),
+          .in(genaddstg[i-1].sum),
+          .out(temp));
+      end
+      
+      else begin
+        logic signed [INPUT_WIDTH+i:0] sum [0:((NUM_INPUTS+(2**(i+1))-1)/(2**(i+1)))-1];
+        adder_tree_stage #(
+          .NUM_INPUTS((NUM_INPUTS+(2**i)-1)/(2**i)), //number of inputs for that stage
+          .ACCUMULATOR_WIDTH(INPUT_WIDTH+i+1), //each stage has an extra bit (to account for double the range during add)
+          .INPUT_WIDTH(INPUT_WIDTH+i)) //input width for that stage
+        adder_stage(
+          .clk(clk),
+          .arst_n_in(arst_n_in),
+          .in(genaddstg[i-1].sum),
+          .out(sum));
+      end
+      
+    end : genaddstg
   endgenerate
-  
-//NOTE: Vivado cannot resolve hierarchical names. Alternative requires connecting signals by reference to generated blocks
-//  //binary tree adder buffers for each stage
-//  generate
-//    for(genvar n=0; n<=$clog2(NUM_INPUTS); n=n+1) begin : adder_stage_buffer //7 nets for 6 stage tree, 0 <= n <= 6
-//      for(genvar i=0; i<((NUM_INPUTS+(2**n)-1)/(2**n)); i=i+1) begin : adder_stage_buffer_inst //each signal in that stage
-//        `REG(INPUT_WIDTH+n, partial_sum); //register in the critical path between adder stages for each operand
-//        assign partial_sum_we = 1'b1; //register always enabled
-//      end : adder_stage_buffer_inst
-//    end : adder_stage_buffer
-//  endgenerate
-  
-//  always @(*) begin
-//    for(int j=0; j<$clog2(NUM_INPUTS); j=j+1)
-//      for(int i=0; i<NUM_INPUTS; i=i+1) begin 
-//        adder_stage_buffer[j].adder_stage_buffer_inst[i].partial_sum_next = in[i]; //connect first net to input of adder tree
-//      end
-    
-//    assign out = nets[$clog2(NUM_INPUTS)].sum; //connect module output to output of adder tree
-//  end
-//  assign adder_stage_buffer[0].adder_stage_buffer_inst[].partial_sum_next = in[i];
-
 endmodule
